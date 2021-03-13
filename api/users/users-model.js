e3d994aa24d58e00a053f8cfbcdab32453503edf
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-03-12 18:32:54 -0700

    finished user model functions

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,6 +18,9 @@ function find() {
       }
     ]
    */
+	return db("users as u")
+		.innerJoin("roles as r", "u.role_id", "r.role_id")
+		.select("u.user_id", "u.username", "r.role_name")
 }
 
 function findBy(filter) {
@@ -34,6 +37,10 @@ function findBy(filter) {
       }
     ]
    */
+	return db("users as u")
+		.innerJoin("roles as r", "u.role_id", "r.role_id")
+		.select("u.*", "r.role_name")
+		.where(filter) // how would this work if the filter has nothing to compare to?
 }
 
 function findById(user_id) {
@@ -47,6 +54,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+	return db('users as u')
+		.innerJoin("roles as r", "u.role_id", "r.role_id")
+		.where("u.user_id", user_id)
+		.first("u.user_id", "u.username", "r.role_name")
 }
 
 /**
